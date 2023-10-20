f24f9bc4f091bf5623b9edf9b7de51e74e5336b4
Ahmed Ashraf <53544989+AhmedAsh4@users.noreply.github.com>
2023-10-20 22:22:22 +0300

    Initial Upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      width: {
+        'custom': '47rem',},
+      colors: {
+        'custom-gray': '#C0C0C0'
+      },
+    },
+  },
+  plugins: [],
+}
+
